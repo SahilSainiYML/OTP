3db2912d5f730b5653322fad6504d305ed046d05
sahilsainiyml <sahil.saini@ymedialabs.com>
2024-01-11 13:36:44 +0530

    removed debugger and log; added support for delete

--- script.js
+++ script.js
@@ -2,15 +2,12 @@ const input1 = document.querySelector(".input:nth-child(1)");
 const input4 = document.querySelector(".input:nth-child(4)");
 const inputs = document.getElementById("inputs");
 document.addEventListener("keyup", (e) => {
-  console.log(e.key);
-  debugger;
-  if (e.key === "Backspace") {
+  if (e.key === "Backspace" || e.key === "Delete") {
     e.target.value = "";
     if (e.target !== input1) {
       e.target.previousElementSibling.focus();
     }
   } else {
-    console.log(e.key);
     if (e.target !== input4) {
       e.target.nextElementSibling.focus();
     }
